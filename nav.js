96dd86e1d4c8ac8ff5ca1ba645d21218588c85b5
smertmozga <54936611+smertmozga@users.noreply.github.com>
2019-09-16 20:16:09 +0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,10 +1,10 @@
  jQuery(function($) {
 	        $(window).scroll(function(){
-	            if($(this).scrollTop()>150){
+	            if($(this).scrollTop()>100){
 	                $('#navigation').addClass('fixed');
 	                $('#menu a').addClass('fixed1');
 	            }
-	            else if ($(this).scrollTop()<155){
+	            else if ($(this).scrollTop()<105){
 	                $('#navigation').removeClass('fixed');
 	                $('#menu a').removeClass('fixed1');
 	            }
